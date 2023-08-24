edd1977e9a76bf1cdb099126bf4846385b63ff9b
Uche Chris <chrisuche58@yahoo.com>
2023-08-24 13:08:06 +0100

    placeholder

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Button, Image } from '@rneui/base'
+import { Button, Image, Input } from '@rneui/base'
 import { StatusBar } from 'expo-status-bar'
 
 
@@ -12,10 +12,19 @@ const LoginScreen = () => {
             source={require("../image/chat.jpg")}
             style={{ width: 200, height: 200 }}
          />
+
+         <View style={styles.inputContainer}>
+            <Input placeholder='Email' autoFocus type="email" />
+            <Input placeholder='Password' secureTextEntry type="password" />
+         </View>
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputContainer: {
+
+    }
+})
