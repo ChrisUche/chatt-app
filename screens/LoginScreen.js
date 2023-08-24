e84f02ad445326c3f13aeeba23e7cc558579849d
Uche Chris <chrisuche58@yahoo.com>
2023-08-24 12:34:07 +0100

    loginscreen begin

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Button } from '@rneui/base'
+import { StatusBar } from 'expo-status-bar'
+
 
 const LoginScreen = () => {
   return (
     <View>
+        <StatusBar style='auto'/>
       <Text>LoginScreen</Text>
     </View>
   )
