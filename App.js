4cc01655b7fd3c542fa6cff3efa8e6e4401bef9f
Uche Chris <chrisuche58@yahoo.com>
2023-08-24 12:12:43 +0100

    update

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={LoginScreen} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnzGgvZ70VHnn-YfZfMelgVCnqSPKSZFM",
+    authDomain: "chatt-app-b0607.firebaseapp.com",
+    projectId: "chatt-app-b0607",
+    storageBucket: "chatt-app-b0607.appspot.com",
+    messagingSenderId: "296667104998",
+    appId: "1:296667104998:web:c005c60a2287419d745d27"
+  };
